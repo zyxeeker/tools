836bd4dab3ebefe82c61afd969940a9915b9ce3b
zyxeeker <zyxeeker@gmail.com>
2024-04-24 17:30:11 +0800

    :construction: 增加观察者模板

--- include/pattern.hpp
+++ include/pattern.hpp
@@ -2,13 +2,14 @@
  * @Author: zyxeeker zyxeeker@gmail.com
  * @Date: 2024-04-24 15:00:06
  * @LastEditors: zyxeeker zyxeeker@gmail.com
- * @LastEditTime: 2024-04-24 15:41:10
+ * @LastEditTime: 2024-04-24 17:29:47
  * @Description: 
  */
 
 #ifndef PATTERN_HPP_
 #define PATTERN_HPP_
 
+#include <map>
 #include <memory>
 
 namespace tools {
@@ -40,6 +41,38 @@ class LazySingleton {
     static std::unique_ptr<T> inst = std::make_unique<T>();
     return *inst;
   }
+};
+
+template <typename T>
+class Observer {
+ public:
+  class Listener {
+   public:
+    using Id = size_t;
+    using WPtr = std::weak_ptr<Listener>;
+    Listener() = default;
+    virtual ~Listener() = default;
+    virtual void Handle(const T& val) = 0;
+  };
+ public:
+  Observer() = default;
+  virtual ~Observer() = default;
+  inline typename Listener::Id RegisterListener(typename Listener::WPtr listener) {
+    static typename Listener::Id id = 0;
+    listeners_.insert({id++, std::move(listener)});
+    return id;
+  }
+  inline void UnregisterListener(typename Listener::Id id) {
+    listeners_.erase(id);
+  }
+  void DispatchData(const T& val) {
+    for (auto& i : listeners_) {
+      if (i.second.expired()) continue;
+      i.second.lock()->Handle(val);
+    }
+  }
+ private:
+  std::map<typename Listener::Id, typename Listener::WPtr> listeners_;
 };
 
 } // pattern
--- tests/test_pattern.cpp
+++ tests/test_pattern.cpp
@@ -2,14 +2,40 @@
 // Created by zyxeeker on 2024/4/24.
 //
 
-#include <iostream>
+#include <log.h>
 #include <pattern.hpp>
 
 class A {
  public:
   void Test(int num) {
-    std::cout << "TEST#" << num << std::endl;
+    LOG_D() << "TEST#" << num << std::endl;
   }
+};
+
+class Elem : public tools::pattern::Observer<int> {
+ public:
+  Elem() = default;
+  ~Elem() = default;
+  inline void set_num(int num) {
+    LOG_D() << "set num: " << num << std::endl;
+    num_ = num;
+    DispatchData(num_);
+  }
+  inline int num() const { return num_; }
+ private:
+  int num_{0};
+};
+
+class ElemListener : public tools::pattern::Observer<int>::Listener {
+ public:
+  explicit ElemListener(const char* name)
+      : name_(name) {}
+  ~ElemListener() = default;
+  void Handle(const int& val) override {
+    LOG_D() << name_ << " handle update: " << val << std::endl;
+  }
+ private:
+  const char* name_;
 };
 
 int main() {
@@ -17,4 +43,14 @@ int main() {
   using LSMgr = tools::pattern::LazySingleton<A>;
   HSMgr::GetInstance().Test(1);
   LSMgr::GetInstance().Test(2);
+
+  auto elem = std::make_unique<Elem>();
+  auto elem_listener_1 = std::make_shared<ElemListener>("elem listener 1");
+  auto elem_listener_2 = std::make_shared<ElemListener>("elem listener 2");
+  elem->RegisterListener(elem_listener_1);
+  elem->RegisterListener(elem_listener_2);
+
+  elem->set_num(1);
+  elem->set_num(2);
+  elem->set_num(3);
 }
